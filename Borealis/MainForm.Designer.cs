e2f8bbede88fce593784cd0284bbd48d6ad8698c
a-trama <trama.antonio@gmail.com>
2018-08-19 22:28:03 +0200

    Experimental branch

--- Borealis/MainForm.Designer.cs
+++ Borealis/MainForm.Designer.cs
@@ -30,9 +30,9 @@
         {
             this.helpOpenFormButton = new System.Windows.Forms.Button();
             this.settingsOpenFormButton = new System.Windows.Forms.Button();
-            this.CMDConsole = new ConsoleControl.ConsoleControl();
             this.InitButton = new System.Windows.Forms.Button();
             this.StartSSHButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // helpOpenFormButton
@@ -55,16 +55,6 @@
             this.settingsOpenFormButton.UseVisualStyleBackColor = true;
             this.settingsOpenFormButton.Click += new System.EventHandler(this.settingsOpenFormButton_Click);
             // 
-            // CMDConsole
-            // 
-            this.CMDConsole.IsInputEnabled = true;
-            this.CMDConsole.Location = new System.Drawing.Point(13, 13);
-            this.CMDConsole.Name = "CMDConsole";
-            this.CMDConsole.SendKeyboardCommandsToProcess = false;
-            this.CMDConsole.ShowDiagnostics = false;
-            this.CMDConsole.Size = new System.Drawing.Size(679, 392);
-            this.CMDConsole.TabIndex = 14;
-            // 
             // InitButton
             // 
             this.InitButton.Location = new System.Drawing.Point(698, 295);
@@ -85,15 +75,26 @@
             this.StartSSHButton.UseVisualStyleBackColor = true;
             this.StartSSHButton.Click += new System.EventHandler(this.StartSSHButton_Click);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.richTextBox1.Location = new System.Drawing.Point(13, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(679, 392);
+            this.richTextBox1.TabIndex = 17;
+            this.richTextBox1.Text = "\n\n\n\n\n\n\n\n\n\n\n\n\n                                                                    " +
+    "                               WELCOME";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(780, 418);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.StartSSHButton);
             this.Controls.Add(this.InitButton);
-            this.Controls.Add(this.CMDConsole);
             this.Controls.Add(this.settingsOpenFormButton);
             this.Controls.Add(this.helpOpenFormButton);
             this.Name = "MainForm";
@@ -106,9 +107,9 @@
 
         private System.Windows.Forms.Button helpOpenFormButton;
         private System.Windows.Forms.Button settingsOpenFormButton;
-        private ConsoleControl.ConsoleControl CMDConsole;
         private System.Windows.Forms.Button InitButton;
         private System.Windows.Forms.Button StartSSHButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
